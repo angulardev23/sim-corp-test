88ee7999217eb0f4837acf8d2577789d458475b3
Kamil Gancarczyk <kamil.gancarczyk@taxually.com>
2026-08-05 14:14:37 +0100

    Product Repo

---
+++ Backend-Test/src/BackendTest.Infrastructure/Persistence/ProductRepository.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BackendTest.Application.Repositories;
+using BackendTest.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendTest.Infrastructure.Persistence
+{
+    public sealed class ProductRepository : IProductRepository
+    {
+        private readonly BackendTestDbContext _context;
+
+        public ProductRepository(BackendTestDbContext context) => _context = context;
+
+        public async Task<IReadOnlyList<Product>> GetAllAsync(CancellationToken cancellationToken) =>
+            await _context.Products.AsNoTracking()
+                .Select(product => new Product(product.Id, product.Name, product.Type, product.Price))
+                .ToArrayAsync(cancellationToken);
+
+        public async Task<Product?> FindByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            var product = await _context.Products.AsNoTracking()
+                .SingleOrDefaultAsync(item => item.Id == id, cancellationToken);
+            return product is null ? null : new Product(product.Id, product.Name, product.Type, product.Price);
+        }
+
+        public async Task AddAsync(Product product, CancellationToken cancellationToken)
+        {
+            _context.Products.Add(ToRecord(product));
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task ReplaceAsync(Product product, CancellationToken cancellationToken)
+        {
+            _context.Products.Update(ToRecord(product));
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task RemoveAsync(Product product, CancellationToken cancellationToken)
+        {
+            _context.Products.Remove(ToRecord(product));
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        private static ProductRecord ToRecord(Product product) => new()
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Type = product.Type,
+            Price = product.Price
+        };
+    }
+}
